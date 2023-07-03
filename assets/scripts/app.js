3a1c79ff575c809395d4237e07ca9238de68fcfd
hoangpham27 <pvh2772002@gmail.com>
2023-07-03 22:59:41 +0700

    Section pictrues-videos

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -289,3 +289,28 @@ productList.forEach((product) => {
         removeFromCart();
     });
 });
+
+// Lightbox gallery
+$(document).ready(function () {
+    $(".gallery-pictures").magnificPopup({
+        delegate: "a",
+        type: "image",
+        tLoading: "Loading image #%curr%...",
+        mainClass: "mfp-img-mobile",
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function (element) {
+                return element.find("img");
+            },
+        },
+    });
+});
