603f77f71fb13cbc487a6e4e372d48552efdb3a9
hoangpham27 <pvh2772002@gmail.com>
2023-08-04 15:35:47 +0700

    View PDF in mobile without downloading

--- assets/scripts/cer-ann.js
+++ assets/scripts/cer-ann.js
@@ -3,7 +3,6 @@ const rowCerAnn = document.querySelector(".row-cer-ann");
 const allItem = rowCerAnn.querySelectorAll(".row-cer-ann .item");
 
 allItem.forEach((item) => {
-    console.log(item.childNodes[3]);
-    item.childNodes[3].style.height = item.clientWidth * (29.7 / 21) + "px";
-    console.log(item.childNodes[3].clientHeight);
+    item.querySelector("iframe").style.height =
+        item.clientWidth * (29.7 / 21) - 50 + "px";
 });
